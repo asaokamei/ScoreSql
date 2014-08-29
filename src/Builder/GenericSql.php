c9fe0922973d2318b8289fae5fecfa26e06b83f2
Asao <kamei@workspot.jp>
2014-08-30 08:56:23 +0900

    extract generic utility methods from GenericSql to GenericUtils class.

--- src/Builder/GenericSql.php
+++ src/Builder/GenericSql.php
@@ -3,31 +3,9 @@ namespace WScore\ScoreSql\Builder;
 
 use WScore\ScoreSql\Sql\Join;
 use WScore\ScoreSql\Sql\Sql;
-use WScore\ScoreSql\Sql\SqlInterface;
-use WScore\ScoreSql\Sql\Where;
 
-class GenericSql
+class GenericSql extends GenericUtils
 {
-    /**
-     * @var Bind
-     */
-    protected $bind = null;
-
-    /**
-     * @var Quote
-     */
-    protected $quote = null;
-
-    /**
-     * @var Builder
-     */
-    protected $builder = null;
-
-    /**
-     * @var string
-     */
-    protected $quoteChar = '"';
-
     /**
      * @var Sql
      */
@@ -76,87 +54,6 @@ class GenericSql
         'where',
     ];
 
-    // +----------------------------------------------------------------------+
-    //  construction
-    // +----------------------------------------------------------------------+
-    /**
-     * @param Bind  $bind
-     * @param Quote $quote
-     * @param Builder $builder
-     * @param Sql $query
-     */
-    public function __construct( $bind, $quote, $builder, $query )
-    {
-        $this->quote = $quote;
-        $this->quote->setQuote( $this->quoteChar );
-        $this->bind  = $bind;
-        $this->builder = $builder;
-        $this->query = $query;
-    }
-
-    /**
-     * @param string $key
-     * @return mixed
-     */
-    protected function getMagicQuery( $key )
-    {
-        if( $key == 'getAliasOrTable' ) {
-            return $this->query->magicGet('tableAlias') ?: $this->query->magicGet('table');
-        }
-        return $this->query->magicGet( $key );
-    }
-
-    /**
-     * @param string|array $name
-     * @param string $alias
-     * @return string
-     */
-    protected function quote( $name, $alias=null )
-    {
-        if( !$this->quote ) return $name;
-        if( is_array( $name ) ) return $this->quote->map( $name );
-        return $this->quote->quote( $name, $alias );
-    }
-
-    /**
-     * @param string|array $val
-     * @param null|int|string  $col     column name. used to find data type
-     * @param null|int         $type    data type
-     * @return array|string
-     */
-    protected function prepare( $val, $col, $type=null )
-    {
-        return $this->bind->prepare( $val, $col, $type );
-    }
-    // +----------------------------------------------------------------------+
-    //  builders
-    // +----------------------------------------------------------------------+
-    /**
-     * @param string $sqlType
-     * @return string
-     */
-    public function build($sqlType)
-    {
-        $list = $this->$sqlType;
-        return $this->buildByList($list);
-    }
-    
-    /**
-     * @param $list
-     * @return string
-     */
-    protected function buildByList( $list )
-    {
-        $statement = '';
-        foreach ( $list as $item ) {
-            $method = 'build' . ucwords( $item );
-            if ( $sql = $this->$method() ) {
-                $statement .= ' ' . $sql;
-            }
-        }
-        return $statement;
-    }
-
     /**
      * @return string
      */
@@ -199,20 +96,6 @@ class GenericSql
         );
     }
 
-    /**
-     * @param array    $list
-     * @param \Closure $callable
-     * @param string   $sep
-     * @return string
-     */
-    protected function formatList( $list, $callable, $sep=', ' )
-    {
-        foreach( $list as $col => $val ) {
-            $list[$col] = $callable( $col, $val );
-        }
-        return implode( $sep, $list );
-    }
-    
     /**
      * @return string
      */
@@ -327,7 +210,7 @@ class GenericSql
      */
     protected function buildLimit()
     {
-        return $this->buildLimitOrOffset('limit');
+        return $this->formatInteger('limit');
     }
 
     /**
@@ -335,15 +218,7 @@ class GenericSql
      */
     protected function buildOffset()
     {
-        return $this->buildLimitOrOffset('offset');
-    }
-
-    protected function buildLimitOrOffset($type)
-    {
-        if( !$integer = $this->getMagicQuery($type) ) return '';
-        if( !is_numeric( $integer ) ) return '';
-        if( $integer <= 0 ) return '';
-        return strtoupper($type) . " " . $integer;
+        return $this->formatInteger('offset');
     }
 
     /**
@@ -357,20 +232,6 @@ class GenericSql
         return '';
     }
 
-    /**
-     * @param mixed $string
-     * @return string
-     */
-    protected function evaluate( $string )
-    {
-        if( is_callable($string) ) {
-            return $string();
-        } elseif( is_object($string) && $string instanceof SqlInterface ) {
-            $builder = new Builder( $this->bind, $this->quote );
-            return '( '.$builder->toSql( $string ).' )';
-        }
-        return null;
-    }
     // +----------------------------------------------------------------------+
     //  builders for where clause.
     // +----------------------------------------------------------------------+
@@ -379,7 +240,7 @@ class GenericSql
      */
     protected function buildWhere()
     {
-        $criteria = $this->buildCriteria( 'where' );
+        $criteria = $this->formatCriteria( 'where' );
         return $criteria ? 'WHERE '.$criteria : '';
     }
 
@@ -388,24 +249,8 @@ class GenericSql
      */
     protected function buildHaving()
     {
-        $criteria = $this->buildCriteria( 'having' );
+        $criteria = $this->formatCriteria( 'having' );
         return $criteria ? 'HAVING '.$criteria : '';
-    }
-
-    /**
-     * @param string $type
-     * @throws \InvalidArgumentException
-     * @return string
-     */
-    protected function buildCriteria( $type )
-    {
-        if( !$criteria = $this->getMagicQuery($type) ) return '';
-        if( !$criteria instanceof Where ) {
-            throw new \InvalidArgumentException;
-        }
-        $criteria->setBuilder( $this->builder );
-        $sql = $criteria->build( $this->bind, $this->quote, $this->getMagicQuery('tableAlias'), $this->getMagicQuery('tableParent') );
-        return $sql;
     }
     // +----------------------------------------------------------------------+
 }
---
+++ src/Builder/GenericUtils.php
@@ -0,0 +1,165 @@
+<?php
+namespace WScore\ScoreSql\Builder;
+
+use WScore\ScoreSql\Sql\Sql;
+use WScore\ScoreSql\Sql\SqlInterface;
+use WScore\ScoreSql\Sql\Where;
+
+class GenericUtils
+{
+    /**
+     * @var Bind
+     */
+    protected $bind = null;
+
+    /**
+     * @var Quote
+     */
+    protected $quote = null;
+
+    /**
+     * @var Builder
+     */
+    protected $builder = null;
+
+    /**
+     * @var string
+     */
+    protected $quoteChar = '"';
+
+    // +----------------------------------------------------------------------+
+    //  construction
+    // +----------------------------------------------------------------------+
+    /**
+     * @param Bind  $bind
+     * @param Quote $quote
+     * @param Builder $builder
+     * @param Sql $query
+     */
+    public function __construct( $bind, $quote, $builder, $query )
+    {
+        $this->quote = $quote;
+        $this->quote->setQuote( $this->quoteChar );
+        $this->bind  = $bind;
+        $this->builder = $builder;
+        $this->query = $query;
+    }
+
+    /**
+     * @param string $key
+     * @return mixed
+     */
+    protected function getMagicQuery( $key )
+    {
+        if( $key == 'getAliasOrTable' ) {
+            return $this->query->magicGet('tableAlias') ?: $this->query->magicGet('table');
+        }
+        return $this->query->magicGet( $key );
+    }
+
+    /**
+     * @param string|array $name
+     * @param string $alias
+     * @return string
+     */
+    protected function quote( $name, $alias=null )
+    {
+        if( !$this->quote ) return $name;
+        if( is_array( $name ) ) return $this->quote->map( $name );
+        return $this->quote->quote( $name, $alias );
+    }
+
+    /**
+     * @param string|array $val
+     * @param null|int|string  $col     column name. used to find data type
+     * @param null|int         $type    data type
+     * @return array|string
+     */
+    protected function prepare( $val, $col, $type=null )
+    {
+        return $this->bind->prepare( $val, $col, $type );
+    }
+
+    /**
+     * @param mixed $string
+     * @return string
+     */
+    protected function evaluate( $string )
+    {
+        if( is_callable($string) ) {
+            return $string();
+        } elseif( is_object($string) && $string instanceof SqlInterface ) {
+            $builder = new Builder( $this->bind, $this->quote );
+            return '( '.$builder->toSql( $string ).' )';
+        }
+        return null;
+    }
+
+    // +----------------------------------------------------------------------+
+    //  builders
+    // +----------------------------------------------------------------------+
+    /**
+     * @param string $sqlType
+     * @return string
+     */
+    public function build($sqlType)
+    {
+        $list = $this->$sqlType;
+        return $this->buildByList($list);
+    }
+
+    /**
+     * @param $list
+     * @return string
+     */
+    protected function buildByList( $list )
+    {
+        $statement = '';
+        foreach ( $list as $item ) {
+            $method = 'build' . ucwords( $item );
+            if ( $sql = $this->$method() ) {
+                $statement .= ' ' . $sql;
+            }
+        }
+        return $statement;
+    }
+
+    /**
+     * @param array    $list
+     * @param \Closure $callable
+     * @param string   $sep
+     * @return string
+     */
+    protected function formatList( $list, $callable, $sep=', ' )
+    {
+        foreach( $list as $col => $val ) {
+            $list[$col] = $callable( $col, $val );
+        }
+        return implode( $sep, $list );
+    }
+
+    /**
+     * @param string $type
+     * @throws \InvalidArgumentException
+     * @return string
+     */
+    protected function formatCriteria( $type )
+    {
+        if( !$criteria = $this->getMagicQuery($type) ) return '';
+        if( !$criteria instanceof Where ) {
+            throw new \InvalidArgumentException;
+        }
+        $criteria->setBuilder( $this->builder );
+        $sql = $criteria->build( $this->bind, $this->quote, $this->getMagicQuery('tableAlias'), $this->getMagicQuery('tableParent') );
+        return $sql;
+    }
+
+    protected function formatInteger($type)
+    {
+        if( !$integer = $this->getMagicQuery($type) ) return '';
+        if( !is_numeric( $integer ) ) return '';
+        if( $integer <= 0 ) return '';
+        return strtoupper($type) . " " . $integer;
+    }
+
+}
